50feb851ab57271614ce5af9a2b15b62eb9b63b9
ejanes <eddiejanes2504@gmail.com>
2023-09-16 15:46:04 -0400

    "added directMessage.service.js"

---
+++ services/directMessage.service.js
@@ -0,0 +1,86 @@
+"use strict";
+
+/**
+ * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+require("dotenv").config();
+const DbService = require("moleculer-db");
+const MongooseAdapter = require("moleculer-db-adapter-mongoose");
+
+const { Model } = require("../models/business.model");
+
+/** @type {ServiceSchema} */
+module.exports = {
+	name: "directMessage",
+
+	mixins: [DbService],
+
+	adapter: new MongooseAdapter(process.env.MONGO_URI),
+
+	model: Model,
+
+	/**
+	 * Settings
+	 */
+	settings: {},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		create: {
+			rest: {
+				method: "POST",
+				path: "/directMessage",
+			},
+			params: {
+				to: "string",
+				subject: "string",
+				messageDraft: "string",
+			},
+			async handler(ctx) {
+				const entity = ctx.params;
+				return this.adapter
+					.insert(entity)
+					.then((doc) => this.transformDocuments(ctx, {}, doc));
+			},
+		},
+	},
+
+	/**
+	 * Events
+	 */
+	events: {},
+
+	/**
+	 * Methods
+	 */
+	methods: {},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {},
+};
